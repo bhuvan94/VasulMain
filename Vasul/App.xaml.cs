69d1834d9e708b97efaaa6e36e5a822686894a17
Bhuvanesh Gunasekaran <gbhuvan.94@gmail.com>
2023-03-31 17:59:22 +0530

    First

---
+++ Vasul/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Vasul;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Vasul/Model/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Vasul.Model
+{
+    public class Person
+    {
+        public Guid PersonId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
